efc0f73d4bcff91f06a9341223acf96304dc3586
MARTIN-ODHIAMBO <+>
2023-09-15 11:45:00 +0300

    list-comprehension completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    return [even for even in num_list if even % 2 == 0]  
     pass
 
 def make_exclamation(sentence_list):
+    return [f"{string}!" for string in sentence_list]
     pass
